a543cbd7ef3e4c67ae52da25633bdabe6534e605
Gukss <dongkuk97@naver.com>
2023-04-18 11:08:27 +0900

    structure: 프로젝트 init

---
+++ fri/src/main/java/com/project/fri/FriApplication.java
@@ -0,0 +1,13 @@
+package com.project.fri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriApplication.class, args);
+	}
+
+}
