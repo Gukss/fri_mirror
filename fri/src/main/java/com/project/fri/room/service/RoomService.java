bf31ed3282133b0bee0c2ff62832ec05a226cb59
김동국 <dongkuk97@naver.com>
2023-04-19 14:16:39 +0900

    feat: findAllByArea 개발 중 areaRepository 받기 위한 병합

--- fri/src/main/java/com/project/fri/room/service/RoomService.java
+++ fri/src/main/java/com/project/fri/room/service/RoomService.java
@@ -1,5 +1,8 @@
 package com.project.fri.room.service;
 
+import com.project.fri.room.entity.Room;
+import java.util.List;
+
 import com.project.fri.room.dto.CreateRoomRequest;
 import com.project.fri.room.dto.CreateRoomResponse;
 
@@ -10,4 +13,10 @@ import com.project.fri.room.dto.CreateRoomResponse;
 public interface RoomService {
   CreateRoomResponse CreateRoom(CreateRoomRequest request);
 
+  /**
+   * desc: 전체 방 리스트를 조회, area에 따라 해당 지역의 방 리스트를 조회한다.
+   * @param areaString
+   * @return 카테고리 별로 묶여있는 전체 방 리스트
+   */
+  List<Room> findAllByArea(String areaString);
 }
