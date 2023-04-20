d7c96fcf2d8f2e4ae4f75479384a54565b4a744b
김동국 <dongkuk97@naver.com>
2023-04-20 11:11:41 +0900

    feat: 방 목록 조회 완료

--- fri/src/main/java/com/project/fri/room/service/RoomServiceImpl.java
+++ fri/src/main/java/com/project/fri/room/service/RoomServiceImpl.java
@@ -1,10 +1,13 @@
 package com.project.fri.room.service;
 
 import com.project.fri.common.entity.Area;
+import com.project.fri.common.entity.Category;
 import com.project.fri.common.repository.AreaRepository;
 import com.project.fri.exception.exceptino_message.NotFoundExceptionMessage;
 import com.project.fri.room.dto.CreateRoomRequest;
 import com.project.fri.room.dto.CreateRoomResponse;
+import com.project.fri.room.dto.FindAllRoomInstance;
+import com.project.fri.room.dto.FindAllRoomResponse;
 import com.project.fri.room.entity.Room;
 import com.project.fri.room.entity.RoomCategory;
 import com.project.fri.room.repository.RoomCategoryRepository;
@@ -45,6 +48,7 @@ public class RoomServiceImpl implements RoomService {
 
   /**
    * 방 생성
+   *
    * @param request
    * @return 만든 방 제목
    */
@@ -91,10 +95,72 @@ public class RoomServiceImpl implements RoomService {
   }
 
   @Override
-  public List<Room> findAllByArea(String areaString) {
-//    return roomRepository.findAllByArea(area)
-//        .orElseThrow(
-//            () -> new NotFoundExceptionMessage(NotFoundExceptionMessage.NOT_FOUND_ROOM_LIST));
-    return null;
+  public FindAllRoomResponse findAllByArea(Category areaString) {
+    Area foundArea = areaRepository.findByCategory(areaString);
+
+    //todo: login 완료되면 id 말고 다른 값으로 찾을꺼같다. => 바꿔주기
+    User foundUser = userRepository.findById(1L)
+        .orElseThrow(() -> new NotFoundExceptionMessage(NotFoundExceptionMessage.NOT_FOUND_USER));
+    Long enrollRoomId = foundUser.getRoom().getId();
+
+    List<Room> roomList = roomRepository.findAllByArea(foundArea)
+        .orElseThrow(
+            () -> new NotFoundExceptionMessage(NotFoundExceptionMessage.NOT_FOUND_ROOM_LIST));
+
+    //roomList 반복하면서 responseEnitity 채워주기
+    List<FindAllRoomInstance> drinkList = new ArrayList<>();
+    List<FindAllRoomInstance> mealList = new ArrayList<>();
+    List<FindAllRoomInstance> gameList = new ArrayList<>();
+    List<FindAllRoomInstance> exerciseList = new ArrayList<>();
+    List<FindAllRoomInstance> studyList = new ArrayList<>();
+    List<FindAllRoomInstance> bettingList = new ArrayList<>();
+    List<FindAllRoomInstance> etcList = new ArrayList<>();
+
+    for(Room x: roomList){
+
+      com.project.fri.room.entity.Category category = x.getRoomCategory().getCategory();
+      if(x.getId() == enrollRoomId){ //참여중인 방이면 화면에 출력되지 않는다.
+        continue;
+      }
+      switch(category){
+        //총 7개 + etc
+        case DRINK:
+          drinkList.add(x.createFindRoomResponse(category));
+          break;
+        case MEAL:
+          mealList.add(x.createFindRoomResponse(category));
+          break;
+        case GAME:
+          gameList.add(x.createFindRoomResponse(category));
+          break;
+        case EXERCISE:
+          exerciseList.add(x.createFindRoomResponse(category));
+          break;
+        case STUDY:
+          studyList.add(x.createFindRoomResponse(category));
+          break;
+        case BETTING:
+          bettingList.add(x.createFindRoomResponse(category));
+          break;
+        case ETC:
+          etcList.add(x.createFindRoomResponse(category));
+          break;
+        default:
+          //todo: 카테고리 없는거 어떻게 할 지 정하기 => 카테고리 없는게 들어올리가 없긴하다. => exceptino으로 던지면 될까?
+          break;
+      }
+    }
+
+    //for문 탈출하면 각 리스트에 값이 다 담겨있다는 의미다.
+    //todo: FindAllRoomResponse에 담아서 ResponseEntity만들어주기
+    return FindAllRoomResponse.builder()
+        .betting(bettingList)
+        .meal(mealList)
+        .stydy(studyList)
+        .etc(etcList)
+        .drink(drinkList)
+        .game(gameList)
+        .exercise(exerciseList)
+        .build();
   }
 }
--- fri/src/main/java/com/project/fri/user/repository/UserRepository.java
+++ fri/src/main/java/com/project/fri/user/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package com.project.fri.user.repository;
 
+import com.project.fri.room.entity.Room;
 import com.project.fri.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * packageName    : com.project.fri.user.repository fileName       : UserRepository date           :
  * 2023-04-18 description    :
  */
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
