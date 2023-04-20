116a1b6f7c40d84ab3c4f8592aa16dbfeccfbd41
JeongBeomi <qja367@gmail.com>
2023-04-20 17:34:49 +0900

    feat: add updateUserRoom DTO

---
+++ fri/src/main/java/com/project/fri/room/dto/UpdateUserRoomRequest.java
@@ -0,0 +1,8 @@
+package com.project.fri.room.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateUserRoomRequest {
+    private Boolean isParticipate;
+}
---
+++ fri/src/main/java/com/project/fri/room/dto/UpdateUserRoomResponse.java
@@ -0,0 +1,4 @@
+package com.project.fri.room.dto;
+
+public class UpdateUserRoomResponse {
+}
