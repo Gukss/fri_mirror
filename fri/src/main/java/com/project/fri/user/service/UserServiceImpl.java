fbd08a20540a7cc8c45b146cdec9810ac60350b7
Gukss <dongkuk97@naver.com>
2023-05-01 11:33:43 +0900

    feat: chromedriver 경로 수정

--- fri/src/main/java/com/project/fri/user/service/UserServiceImpl.java
+++ fri/src/main/java/com/project/fri/user/service/UserServiceImpl.java
@@ -47,7 +47,7 @@ public class UserServiceImpl implements UserService {
 
   private static final String URL = "https://edu.ssafy.com/comm/login/SecurityLoginForm.do";
   private static final String LOCAL_PATH = "D:\\Guk\\fri\\chromedriver.exe";
-  private static final String SERVER_PATH = "/usr/bin/chromedriver";
+  private static final String SERVER_PATH = "/home/ubuntu/chromedriver";
 
   @Override
   public User findById(long userId) {
