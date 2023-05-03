8594df2e3937449749fef531d7c89734d640b961
eunnu <angellina9413@gmail.com>
2023-05-03 15:07:16 +0900

    feat: signup axios

--- fri/src/pages/My/MyPage.tsx
+++ fri/src/pages/My/MyPage.tsx
@@ -1,15 +1,43 @@
-import { useState } from "react";
-import Nav from "../../components/navEgg"
-import my from "../../assets/my_img.png"
-import plus from "../../assets/plus_btn.png"
-import egg from "../../assets/egg.png"
-import logo from "../../assets/images/Logo.png"
-import Back from "../../components/Back"
+import { useState, useEffect } from "react";
+import Nav from "../../components/navEgg";
+import my from "../../assets/my_img.png";
+import plus from "../../assets/plus_btn.png";
+import egg from "../../assets/egg.png";
+import logo from "../../assets/images/Logo.png";
+import Back from "../../components/Back";
+import axios from "axios";
 import "./my.scss"
+
+interface myData {
+  area: string,
+  email : string,
+  gameRoomId : string,
+  major : boolean,
+  name : string,
+  nickname : string,
+  profileUrl : string,
+  roomId : string,
+  year : string
+}
 
 function MyPage() {
 
   const [isnav, setIsnav] = useState(false);
+  const [data, setData] = useState<myData>();
+  const [isget, setGet] = useState<boolean>(false);
+  const api_url = process.env.REACT_APP_REST_API;
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get(api_url + "user", {headers : {"Authorization" : 1}});
+      setData(res.data)
+    }
+    getData();
+  }, [])
+
+  // function getImg() {
+    
+  // }
 
   return (
     <div className="my">
@@ -17,8 +45,14 @@ function MyPage() {
       <img src={logo} alt="logo" />
       <p>마잉 페이지</p>
       <img src={my} alt="profile" id="profile"/>
-      <img src={plus} alt="plus" id="plus"/>
-      <p id="nick">나는야 감동란 될꺼야</p>
+      <img src={plus} alt="plus" id="plus" onClick={() => setGet(true)}/>
+      {
+        isget ? 
+        <input id="get_img" type="file" accept="image/*;capture=camera"/>
+        :
+        null
+      }
+      <p id="nick">{data?.nickname}</p>
     
       <p id="egg_cnt">현재 달걀 개수</p>
       <div id="eggs">
