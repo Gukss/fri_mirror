06cf2e150cc2b3831d4f52472be207fce9a7fa56
Gukss <dongkuk97@naver.com>
2023-05-04 13:32:30 +0900

    feat: userUpdate 삭제

--- fri/src/main/java/com/project/fri/user/service/UserService.java
+++ fri/src/main/java/com/project/fri/user/service/UserService.java
@@ -13,6 +13,7 @@ public interface UserService {
 
   /**
    * desc: id로 user 찾아오기
+   *
    * @param userId
    * @return
    */
@@ -20,28 +21,24 @@ public interface UserService {
 
   /**
    * 방 입장시 User의 room 업데이트
+   *
    * @param roomId
    * @param request
    * @return
    */
-  ResponseEntity<UpdateUserRoomResponse> updateUserRoom(Long roomId, UpdateUserRoomRequest request, Long userId);
-
-  /**
-   * desc: id로 user 찾아서 들고있는 방 번호가 입력받은 방 번호와 일치하는지 확인하고 일치하면 ready를 not해서 바꿔주기
-   *
-   * @param roomId
-   * @return
-   */
-  UpdateUserReadyResponse updateUserReady(long userId, long roomId);
+  ResponseEntity<UpdateUserRoomResponse> updateUserRoom(Long roomId, UpdateUserRoomRequest request,
+      Long userId);
 
   /**
    * desc: 회원가입
+   *
    * @return
    */
   HttpStatus createUser(CreateUserRequest request);
 
   /**
    * desc: 에듀싸피에 등록된 이메일인지 검증
+   *
    * @param certifiedEduRequest
    * @return
    */
@@ -51,6 +48,7 @@ public interface UserService {
 
   /**
    * desc: 로그인요청시 email, password 확인 맞으면 해당 User 객체, 틀리면 null 반환
+   *
    * @param signInUserRequest
    * @return
    */
@@ -58,6 +56,7 @@ public interface UserService {
 
   /**
    * 유저 정보 조회
+   *
    * @param userId
    * @return
    */
