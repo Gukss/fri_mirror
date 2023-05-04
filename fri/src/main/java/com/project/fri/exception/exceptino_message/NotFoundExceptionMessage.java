c89d38dd04dfbb583292f7aa121028934d1e2351
JeongBeomi <qja367@gmail.com>
2023-05-04 13:27:02 +0900

    feat: add AnonymousProfileImageErrorMessage

--- fri/src/main/java/com/project/fri/exception/exceptino_message/NotFoundExceptionMessage.java
+++ fri/src/main/java/com/project/fri/exception/exceptino_message/NotFoundExceptionMessage.java
@@ -11,6 +11,7 @@ public class NotFoundExceptionMessage extends RuntimeException{
   public static final String NOT_FOUND_AREA = "존재하지 않는 지역입니다.";
   public static final String NOT_FOUND_ROOM_CATEGORY = "존재하지 않는 지역입니다.";
   public static final String NOT_FOUND_CERTIFICATION = "존재하지 않는 검증입니다.";
+  public static final String NOT_FOUND_AnonymousProfileImage = "존재하지 않는 이미지입니다.";
 
   public NotFoundExceptionMessage() {
     super();
