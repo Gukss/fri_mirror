4c5a538d9b59edaf421da24c4cf1341b1c419f99
youngman <sksn12@naver.com>
2023-05-08 17:42:42 +0900

    TEST

--- fri/src/main/java/com/project/fri/chatting/repository/ChattingRepositoryCustom.java
+++ fri/src/main/java/com/project/fri/chatting/repository/ChattingRepositoryCustom.java
@@ -3,8 +3,9 @@ package com.project.fri.chatting.repository;
 import com.project.fri.chatting.dto.FindChattingMessageResponse;
 import com.project.fri.chatting.entity.ChattingMessage;
 import com.project.fri.room.entity.Room;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ChattingRepositoryCustom {
-  List<FindChattingMessageResponse> findChattingMessageAndUser(Long roomId);
+  List<FindChattingMessageResponse> findChattingMessageAndUser(Long roomId, LocalDateTime time);
 }
