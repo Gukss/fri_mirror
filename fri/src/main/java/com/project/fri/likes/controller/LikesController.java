dd673f6af701a3b02e782364afda96a299fb5c4e
JeongBeomi <qja367@gmail.com>
2023-05-11 17:57:06 +0900

    feat: adding createLikes

--- fri/src/main/java/com/project/fri/likes/controller/LikesController.java
+++ fri/src/main/java/com/project/fri/likes/controller/LikesController.java
@@ -1,6 +1,24 @@
 package com.project.fri.likes.controller;
 
+import com.project.fri.likes.service.LikesService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/likes")
+@Slf4j
 public class LikesController {
+    private final LikesService likesService;
+
+    @PostMapping
+    public ResponseEntity<?> createLikes() {
+
+        return null;
+    }
 
 }
