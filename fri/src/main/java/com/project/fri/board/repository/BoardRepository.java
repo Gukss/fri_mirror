cd1f3b84e2d7ef7e3f8714222fa93ed1cbc42a8f
JeongBeomi <qja367@gmail.com>
2023-05-11 21:14:44 +0900

    feat: extend JpaRepository

--- fri/src/main/java/com/project/fri/board/repository/BoardRepository.java
+++ fri/src/main/java/com/project/fri/board/repository/BoardRepository.java
@@ -1,5 +1,8 @@
 package com.project.fri.board.repository;
 
-public interface BoardRepository {
+import com.project.fri.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
 
 }
