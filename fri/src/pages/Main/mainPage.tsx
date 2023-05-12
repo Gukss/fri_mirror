79eee5c4e289b6b7f0777f92eae7512f2410e54c
eunnu <angellina9413@gmail.com>
2023-05-12 15:40:40 +0900

    fix: infinity scroll

--- fri/src/pages/Main/mainPage.tsx
+++ fri/src/pages/Main/mainPage.tsx
@@ -95,6 +95,19 @@ const Main: React.FC = () => {
     });
   }
 
+  // const state = { page_id: 1 };
+  // const title = "Page Title";
+  // const url = "/main";
+  // window.history.pushState(state, title, url);
+
+  // // Add a popstate event listener to handle the back button
+  //     window.addEventListener('popstate', (event) => {
+  //       // Prevent the default back behavior
+  //       // navigate("/main");
+  //       event.preventDefault();
+  //       window.history.pushState(state, title, url);
+  //     })
+
   const changeRegion = async (area: string) => {
     const header = {
       Authorization: Number(userId),
