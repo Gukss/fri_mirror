0e3723183c7a59e7e7229dd8e8fb248afb6d7740
JeongBeomi <qja367@gmail.com>
2023-05-16 02:49:21 +0900

    feat: add createBoard with image

--- fri/src/main/java/com/project/fri/board/service/BoardService.java
+++ fri/src/main/java/com/project/fri/board/service/BoardService.java
@@ -4,8 +4,11 @@ package com.project.fri.board.service;
 import com.project.fri.board.dto.CreateBoardRequest;
 import com.project.fri.board.dto.DeleteBoardRequest;
 import com.project.fri.board.dto.FindBoardResponse;
+
+import java.io.IOException;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface BoardService {
 
@@ -15,7 +18,7 @@ public interface BoardService {
    * @param userId
    * @return
    */
-  void createBoard(CreateBoardRequest createBoardRequest, Long userId);
+  void createBoard(CreateBoardRequest createBoardRequest, List<MultipartFile> boardImage, Long userId);
 
   /**
    * desc: 게시판 카테고리 별 단건 조회
@@ -23,5 +26,7 @@ public interface BoardService {
   List<FindBoardResponse> findBoard();
 
   ResponseEntity deleteBoard(DeleteBoardRequest deleteBoardRequest);
+
+  String createBoardImageUrl(MultipartFile boardImage);
 
 }
