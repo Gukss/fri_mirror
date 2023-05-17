78004ed8fbfb461473ef2c60a2dd77329d8e0621
eunnu <angellina9413@gmail.com>
2023-05-18 00:23:24 +0900

    feat: community detail page and comment create/delete

---
+++ fri/src/components/AgreeModal.tsx
@@ -0,0 +1,58 @@
+import { useNavigate } from "react-router-dom";
+import { RootState } from "../redux/store";
+import { useSelector } from "react-redux";
+import Agree from "./Agree"
+import axios from "axios";
+import "../style_key.scss";
+
+interface modalType {
+  page : string;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function AgreeModal({setOpen, page} : modalType) {
+  const navigate = useNavigate();
+  const api_url = process.env.REACT_APP_REST_API;
+  const userId = useSelector((state: RootState) => {
+    return state.strr.userId;
+  })
+
+  function movePage(check : boolean) {
+    if(page === "login"){
+      const header = {
+        "Content-Type" : "aplication/json",
+        "Authorization" : userId
+      }
+      const data = {
+        "emailAgreement" : check
+      }
+      try {
+        axios.post(api_url + "user/certified/agreement", data, {headers : header})
+        if(check) navigate("/main")
+        else alert("그동안 저희 서비스를 이용해 주셔서 감사합니다. \n 개인정보는 안전하게 삭제되었습니다.")
+      }
+      catch(e){console.log(e)}
+    }
+    else{
+      if(check) navigate("/signup")
+      else alert("마음이 바뀌시면 다시 찾아주세요..")
+    }
+  }
+  
+  return (
+    <div className="agree_background">
+      <div className="agree_modal">
+        <div className="agree_title">이용약관 및 개인정보처리방침</div>
+        <div className="agree_content">
+          <Agree />
+        </div>
+        <div className="Agree_info">비동의 시 서비스 이용이 불가합니다.</div>
+        <div className="modal_btn">
+          <div className="ok_btn" onClick={() => {movePage(true); setOpen(false)}}>동의</div>
+          <div className="no_btn" onClick={() => {movePage(false); setOpen(false)}}>비동의</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default AgreeModal;
--- fri/src/components/Board/CommentList.tsx
+++ fri/src/components/Board/CommentList.tsx
@@ -1,33 +1,28 @@
 import { useRef, useState, useCallback } from "react";
+import { RootState } from "../../redux/store";
+import { useSelector } from "react-redux";
+import { CommentType } from "../../pages/Community/ComDetail";
+import axios from "axios";
 import Up from "../../assets/Arrow upward.png";
 import "./board.scss"
 
-const comment = [
-    {
-        "commentId" : 1,
-        "content" : "맛있긴해", 
-        "createdAt" : "yyyy-mm-dd HH:MM:SS",
-        "Identity" : "writer"
-    },
-    {
-        "commentId" : 2,
-        "content" : "ㅇㅈ",
-        "createdAt" : "yyyy-mm-dd HH:MM:SS",
-        "Identity" : "na"
-    },
-    {
-        "commentId" : 1,
-        "content" : "맛있긴해", 
-        "createdAt" : "yyyy-mm-dd HH:MM:SS",
-        "Identity" : "nam"
-    }
-]
+interface commentType {
+	comment : CommentType[];
+	board : number;
+	cmt : number;
+	setCmt : React.Dispatch<React.SetStateAction<number>>
+}
 
-function CommentList() {
+function CommentList({comment, board, setCmt, cmt} : commentType) {
     const textareaRef = useRef<HTMLTextAreaElement>(null);
     const btnRef = useRef<HTMLButtonElement>(null);
     const [text, setText] = useState<string>("");
-    const [cmt, setData] = useState(comment);
+    const [data, setData] = useState(comment);
+    const api_url = process.env.REACT_APP_TEST_API;
+    const userId = useSelector((state:RootState) => {
+        return state.strr.userId;
+    })
+
     const submitMessage = useCallback(
         (e: React.ChangeEvent<HTMLTextAreaElement>) => {
           setText(e.target.value);
@@ -43,65 +38,92 @@ function CommentList() {
         [text]
       );        
     
+		const delCmt = async (cmtId : number) => {
+			const datas = {
+				"commentId" : cmtId,
+				"isDelete" : true, 
+			}        
+			const header = {
+				"Content-Type" : "application/json",
+				"Authorization" : userId
+			}
+			
+			try {
+				await axios.patch(api_url + 'comment/delete', datas, {headers : header});
+				setData(data.filter((d) => d.commentId !== cmtId))
+				setCmt(cmt-1)
+			}catch(e){console.log(e)}
+		}
 
-    function subCmt() {
-        const data = {
-            "commentId" : 1,
-            "content" : text, 
-            "createdAt" : "yyyy-mm-dd HH:MM:SS",
-            "Identity" : "nam"
-        }
-        
-        setData((prev) => [...prev, data])
-        setText("")
+	const subCmt = async () => {
+		const datas = {
+			"boardId" : board,
+			"content" : text, 
+		}        
+		const header = {
+			"Content-Type" : "application/json",
+			"Authorization" : userId
+		}
+		
+		try {
+			const res = await axios.post(api_url + 'comment', datas, {headers : header});
+			const newData = {...res.data, "identity" : "na"}
+			setData([...data, newData])
+			setCmt(cmt+1)
+		}catch(e){console.log(e)}
+		
+		setText("")
+	}
 
-    }
 
-
-    return (
-        <div className="comment">
-            <div className="comment_box">
-                {
-                    cmt.length ? 
-                    cmt.map((cmt, index) => (
-                        <div className="cmt" key={index}><div className="cmt_name">{
-                            cmt.Identity === "na" && "나" || cmt.Identity === "nam" && `익명${index+1}` || cmt.Identity === "writer" && "작성자"
-                        }</div>{cmt.content}
-                        {
-                            cmt.Identity === "na" &&
-                            <div className="mycmt">
-                                <div className="cmt_r">수정</div>
-                                <div className="cmt_d">삭제</div>
-                            </div>
-                        }
-                        </div>
-                    ))
-                    :
-                    null
-                }
-            </div>
-            <div className="comment_bottom">
-                <div className="text-input">
-                <textarea
-                    ref={textareaRef}
-                    placeholder="댓글 여기에"
-                    onChange={submitMessage}
-                    value={text}
-                />
-                {text !== "" ? (
-                    <button
-                    className="send-message"
-                    ref={btnRef}
-                    onClick={subCmt}
-                    >
-                    <img src={Up} alt="up-arrow" />
-                    </button>
-                ) : (
-                    ""
-                )}
-                </div>
-            </div>
-          </div>
-    )
+	return (
+	<div className="comment">
+		<div className="comment_box">
+			{
+				data.length ? 
+				data.map((cmt, index) => (
+					<div className="cmt" key={index}>
+						<div className="cmt_top">
+						<div className="cmt_name">{
+							cmt.identity === "na" && "나" || cmt.identity === "nam" && `익명${index+1}` || cmt.identity === "writer" && "작성자"}
+						</div>
+					{
+						cmt.identity === "na" &&
+						<div className="mycmt">
+							{/* <div className="cmt_r">수정</div> */}
+							<div className="cmt_d" onClick={() => delCmt(cmt?.commentId)}>삭제</div>
+						</div>
+					}
+				</div>
+					<div className="cmt_content">{cmt.content}</div>
+				</div>
+				))
+				:
+				null
+			}
+			</div>
+			<div className="comment_bottom">
+				<div className="text-input">
+				<textarea
+					ref={textareaRef}
+					placeholder="댓글 여기에"
+					onChange={submitMessage}
+					value={text}
+				/>
+				{text !== "" ? (
+					<button
+					className="send-message"
+					ref={btnRef}
+					onClick={subCmt}
+					>
+					<img src={Up} alt="up-arrow" />
+					</button>
+				) : (
+						""
+				)}
+				</div>
+			</div>
+		</div>
+	)
 }
 export default CommentList;
