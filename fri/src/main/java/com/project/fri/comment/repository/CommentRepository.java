8d8ad4c89ec27a5b34ecea18ee77fb53875f3c56
허정범 <qja367@gmail.com>
2023-05-17 17:00:59 +0900

    feat: findBoardDetail

--- fri/src/main/java/com/project/fri/comment/repository/CommentRepository.java
+++ fri/src/main/java/com/project/fri/comment/repository/CommentRepository.java
@@ -2,9 +2,18 @@ package com.project.fri.comment.repository;
 
 import com.project.fri.board.entity.Board;
 import com.project.fri.comment.entity.Comment;
+import com.project.fri.scrap.entity.Scrap;
+import com.project.fri.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long>,CommentRepositoryCustom {
 
-    int countByBoardAndIsDeleteFalse(Board board);
+    long countByBoardAndIsDeleteFalse(Board board);
+//    @Query("SELECT c FROM Comment c JOIN FETCH c.board.user.id JOIN FETCH c.user.id WHERE c.board = :board And c.isDelete = false ORDER BY c.createdAt DESC")
+    @Query("SELECT c FROM Comment c JOIN FETCH c.board JOIN FETCH c.user WHERE c.board = :board And c.isDelete = false ORDER BY c.createdAt DESC")
+    List<Comment> findAllByBoardAndIsDeleteFalseOrderByCreatedAtDescWithBoardAndUser(@Param("board") Board board);
 }
